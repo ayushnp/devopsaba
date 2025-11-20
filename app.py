583fa07a83addd0617ab63ff602e96bc28a5265e
ayushnp <vvce23cse0245@vvce.ac.in>
2025-11-20 19:53:50 +0530

    new file

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
+API_URL = "https://zenquotes.io/api/random"
+
+@app.route("/")
 def home():
-    return "Hello from CI/CD Pipeline!"
+    try:
+        response = requests.get(API_URL, timeout=5)
+        data = response.json()
+
+        quote = data[0]["q"]
+        author = data[0]["a"]
+
+        return render_template("index.html", quote=quote, author=author)
+
+    except Exception:
+        return render_template("index.html", quote="Could not load quote.", author="API Error")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
