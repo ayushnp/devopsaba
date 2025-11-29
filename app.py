934e332ed3556466033e70e96b01b0bc5cef0185
AYUSH N P <150500888+ayushnp@users.noreply.github.com>
2025-11-29 13:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def dashboard():
         return "<h1>Student Dashboard - View Registered Events here</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
